715d52df07e03fa97510765ab57666826218f6d8
Nick Gomes <ngomes@dreamsailgames.com>
2016-11-16 15:18:19 -0500

    Adding logic to send and receive new game servers!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+var express 			= require('express');
+var app 				= express();
+var gameServerArray 	= new Array();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,9 +10,86 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+
+
+app.get('/', function(request, response) 
+{
+	response.send("test");
 });
+
+
+
+app.get('/list-server', function(request, response) 
+{
+	response.send( JSON.stringify(gameServerArray) );
+});
+
+
+
+
+
+app.post('/join-server', function(request, response) 
+{
+	var serverToJoin = "";
+	
+	if(gameServerArray.length > 0)
+	{
+		serverToJoin = JSON.stringify(gameServerArray[0])
+	}
+	
+	response.send( serverToJoin );
+});
+
+
+
+
+app.post('/game-server-heartbeat', function(request, response) 
+{
+	//TODO: Game servers should have a private key that the master server has a public key version of.
+	//When getting a heartbeat check that the heartbeat is properly signed by a trusted Game Server private key.
+	
+	var jsonString = '';
+	
+	request.on('data', 
+		function(data)
+		{
+			jsonString = data;
+		}
+	);
+	
+	request.on('end', 
+		function () 
+		{
+			var newServerObj 					 = JSON.parse(jsonString);
+			newServerObj.lastHeartbeatTimestamp = Date.now();
+			
+			var isNewServer = true;
+			for(var i=0; i < gameServerArray.length; i++)
+			{
+				var prevServerObj = gameServerArray[i];
+				if(prevServerObj.ip == newServerObj.ip)
+				{
+					prevServerObj.lastHeartbeatTimestamp = Date.now();
+					isNewServer = false;
+					break;
+				}
+			}
+			
+			
+			if(isNewServer)
+			{
+				gameServerArray.push(newServerObj);
+			}
+			
+			response.send("Server Heartbeat received: " + newServerObj.lastHeartbeatTimestamp + " " + newServerObj.ip + " port: " + newServerObj.port);
+		}
+	);
+	
+});
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
